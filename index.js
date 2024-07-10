f74b9491e3ff59cbca23bf97f602ce9ea0970085
Khaled_Almorse <khaledmanea0077@gmail.com>
2024-07-10 15:07:03 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const nodemailer = require("nodemailer");
+
+const transportmail = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "khaledmanea0077@gmail.com",
+    pass: "orcsxpyrrkylrnwq",
+  },
+});
+
+const mailcontent = {
+  from: "khaledmanea0077@gmail.com",
+  to: "mohameadmaneaa22@gmail.com",
+  subject: "Test Email From Khaled Almorse",
+  text: "Hi Khaled Almorse",
+};
+
+transportmail.sendMail(mailcontent, function (err, val) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(val.response, "sent email");
+  }
+});
